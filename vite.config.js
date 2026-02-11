cbc68c6424274e7d1ca305d3609c9399ce23c1dd
TUYIZERE Niyogisubizo Abdon <abdontuyizere@gmail.com>
2026-02-11 22:50:16 +0300

    signin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        products: resolve(__dirname, "src/html/products.html"),
+        categories: resolve(__dirname, "src/html/categories.html"),
+        cart: resolve(__dirname, "src/html/cart.html"),
+        checkout: resolve(__dirname, "src/html/checkout.html"),
+        signin: resolve(__dirname, "src/html/signin.html"),
+        signup: resolve(__dirname, "src/html/signup.html")
+      }
+    }
   }
 });
